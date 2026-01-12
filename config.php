5f4780e5ff72bbd548a9fe7fbbdcd1a72d642fe8
salmawahdi <salmawahdi99@gmail.com>
2026-01-12 22:07:38 +0100

    ajout des modufication

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Le 3ème paramètre (le mot de passe) doit être ""
-$conn = mysqli_connect("localhost", "root", "", "bdd_project");
+$conn = mysqli_connect("localhost", "root", "", "bdd_project",3307);
 
 if (!$conn) {
     die("Erreur de connexion : " . mysqli_connect_error());
