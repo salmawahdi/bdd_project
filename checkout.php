ba7884af1156afd661fe71ca3979faeaefe867ca
AKXdelta <micassabeno@gmail.com>
2026-01-16 18:43:06 +0100

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -319,11 +319,7 @@
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">About Us</h2>
               <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+             
             </div>
           </div>
           <div class="col-lg-4 col-md-6 mb-5 mb-md-5">
